506f18019ded6188cefa0a0b3e947227c04ada03
kch <baram918@naver.com>
2018-04-09 22:07:37 +0900

    안드로이드 회원 삭제 및 관리자 기능 예제 추가

---
+++ Proj_Manager_v7/app/src/main/java/com/example/kch/proj_manager_v7/DeleteRequest.java
@@ -0,0 +1,35 @@
+package com.example.kch.proj_manager_v7;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by kch on 2018. 4. 8..
+ */
+
+public class DeleteRequest extends StringRequest{
+
+    //현재 안드로이드앱을 에뮬레이터로 돌리므로 에뮬레이터가 설치된 서버에 있는 아파치 서버에 접근하려면
+    //다음과 같이 10.0.2.2:포트번호 로 접근해야합니다 저는 8080 포트를 써서 다음과 같이 했습니다
+    final static private String URL = "http://10.0.2.2:8080/Delete.php";
+    private Map<String, String> parameters;
+
+    public DeleteRequest(String userID, Response.Listener<String> listener){
+        super(Request.Method.POST, URL, listener, null);//Post방식임
+        parameters = new HashMap<>();//해쉬맵 생성후 parameters 변수에 값을 넣어줌
+        parameters.put("userID", userID);
+
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return parameters;
+    }
+
+
+}
