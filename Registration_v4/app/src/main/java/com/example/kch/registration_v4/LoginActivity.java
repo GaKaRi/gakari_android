ca6b1b90f31f9f5ac26ffb9ae6561bb5b88a1d3d
kch <baram918@naver.com>
2018-05-15 22:42:26 +0900

    4. 수강신청앱 회원 가입 기능 구현

---
+++ Registration_v4/app/src/main/java/com/example/kch/registration_v4/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.kch.registration_v4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView registerButton = (TextView)findViewById(R.id.registerButton);
+
+        //버튼이 눌리면 RegisterActivity로 가게함
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+    }
+}
---
+++ Registration_v4/app/src/main/java/com/example/kch/registration_v4/ValidateRequest.java
@@ -0,0 +1,29 @@
+package com.example.kch.registration_v4;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by kch on 2018. 5. 14..
+ */
+
+public class ValidateRequest extends StringRequest {
+
+    final static private String URL = "http://10.0.2.2:8080/registration/UserValidate.php";
+    private Map<String, String> parameters;
+
+    public ValidateRequest(String userID, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);//해당 URL에 POST방식으로 파마미터들을 전송함
+        parameters = new HashMap<>();
+        parameters.put("userID", userID);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return parameters;
+    }
+}
